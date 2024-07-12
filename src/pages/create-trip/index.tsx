7f9e27b63fef2264322f80ba463644e9a63168c3
Elina Torres <154446327+elinatorresn@users.noreply.github.com>
2024-07-12 08:53:46 -0300

    feat: create trip returns an ID

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,7 @@ import { ConfirmTripModal } from './confirm-trip-modal'
 import { DestinationAndDateStep } from './steps/destination-and-date-step'
 import { InviteGuestsStep } from './steps/invite-guests-step'
 import { DateRange } from 'react-day-picker'
+import { api } from '../../lib/axios'
 
 export function CreateTripPage() {
   const navigate = useNavigate()
@@ -68,11 +69,38 @@ export function CreateTripPage() {
     setEmailsToInvite(newEmailList)
   }
 
-  function createTrip(event: FormEvent<HTMLFormElement>) {
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
     event.preventDefault()
 
-    console.log(destination, eventStartAndDates, emailsToInvite, ownerName, ownerEmail)
-    // navigate('/trips/123')
+    if (!destination) {
+      return
+    }
+
+    if (!eventStartAndDates?.from || !eventStartAndDates?.to) {
+      return
+    }
+
+    if (emailsToInvite.length === 0) {
+      return
+    }
+
+    if (!ownerName || !ownerEmail) {
+      return
+    }
+
+    const response = await api.post('/trips', {
+      destination,
+      starts_at: eventStartAndDates?.from,
+      ends_at: eventStartAndDates?.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail
+    })
+
+    const { tripId } = response.data
+
+    navigate(`/trips/${tripId}`)
+
   }
 
   return (
